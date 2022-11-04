c6e35973c1b1a87d41bb5b36e06468dbebc1ab12
seon <seonsy44@gmail.com>
2022-11-04 20:42:42 +0900

    modify: 12927

--- seon/level3/12927.js
+++ seon/level3/12927.js
@@ -5,11 +5,15 @@ function solution(n, works) {
   while (n) {
     let idx = 0;
     while (idx < works.length - 1) {
-      if (works[idx] > works[idx + 1]) {
-        works[idx] -= 1;
-        n -= 1;
-        break;
-      } else idx += 1;
+      if (works[idx] <= works[idx + 1]) {
+        idx += 1;
+      } else break;
+    }
+
+    for (let i = idx; i >= 0; i--) {
+      works[i] -= 1;
+      n -= 1;
+      if (!n) break;
     }
 
     if (!works[0]) return 0;
