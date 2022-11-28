c6f393792d8647d962fda5a4278119377f093258
seon <seonsy44@gmail.com>
2022-11-28 10:18:29 +0900

    12980

---
+++ seon/level2/12980.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  let ans = 0;
+  while (n > 0) {
+    if (n % 2) {
+      n -= 1;
+      ans += 1;
+    } else {
+      n /= 2;
+    }
+  }
+  return ans;
+}
+
+console.log(solution(5));
