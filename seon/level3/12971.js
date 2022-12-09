b1d31766e1189f3a6bc9345963b457329249070f
seon <seonsy44@gmail.com>
2022-12-09 21:26:04 +0900

    12971

---
+++ seon/level3/12971.js
@@ -0,0 +1,20 @@
+function solution(sticker) {
+  if (sticker.length <= 2) return Math.max(...sticker);
+
+  const typeA = sticker.slice(0, sticker.length - 1);
+  const numberA = new Array(typeA.length);
+  [numberA[0], numberA[1]] = [typeA[0], Math.max(typeA[0], typeA[1])];
+
+  const typeB = sticker.slice(1);
+  const numberB = new Array(typeB.length);
+  [numberB[0], numberB[1]] = [typeB[0], Math.max(typeB[0], typeB[1])];
+
+  for (let i = 2; i < typeA.length; i++) {
+    numberA[i] = Math.max(numberA[i - 1], numberA[i - 2] + typeA[i]);
+    numberB[i] = Math.max(numberB[i - 1], numberB[i - 2] + typeB[i]);
+  }
+
+  return Math.max(numberA[numberA.length - 1], numberB[numberB.length - 1]);
+}
+
+console.log(solution([14, 6, 5, 11, 3, 9, 2, 10]));
