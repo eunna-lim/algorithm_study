c158f01296a09c4224ae6e914f8801567525c9d1
seon <seonsy44@gmail.com>
2023-04-07 16:46:40 +0900

    42883

---
+++ seon/level2/42883.js
@@ -0,0 +1,14 @@
+function solution(number, k) {
+  const answer = [];
+
+  for (let n of number) {
+    while (k && answer && answer[answer.length - 1] < n) {
+      answer.pop();
+      k -= 1;
+    }
+
+    answer.push(n);
+  }
+
+  return k ? answer.slice(0, answer.length - 1).join("") : answer.join("");
+}
