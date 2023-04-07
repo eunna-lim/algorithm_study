2001539e205b018561aacb5ffe5c5fd8eab3be7f
seon <seonsy44@gmail.com>
2023-04-07 16:48:14 +0900

    42883: 코드 수정

--- seon/level2/42883.js
+++ seon/level2/42883.js
@@ -2,7 +2,7 @@ function solution(number, k) {
   const answer = [];
 
   for (let n of number) {
-    while (k && answer && answer[answer.length - 1] < n) {
+    while (k && answer[answer.length - 1] < n) {
       answer.pop();
       k -= 1;
     }
