8749e2375070596e08e0dceb130583fbbbc5e770
eunna <dladmssk98@gmail.com>
2023-05-10 15:56:25 +0900

    level2 / 178870 - add the 2nd solution

--- eunna/level2/178870.py
+++ eunna/level2/178870.py
@@ -18,3 +18,30 @@ def solution(sequence, k):
     answer.sort(key=lambda x: x[1] - x[0])
 
     return answer[0]
+
+
+def solution2(sequence, k):
+    answer = []
+
+    prefix = [sequence[0]]
+    for i in range(1, len(sequence)):
+        prefix.append(prefix[i - 1] + sequence[i])
+
+    start, end = 0, 0
+    while start <= end and end < len(sequence):
+        # start부터 end까지의 부분합
+        subsum = prefix[end] - prefix[start] + sequence[start]
+
+        if subsum == k:
+            if not answer:
+                answer = [start, end]
+            else:
+                answer = [start, end] if end - \
+                    start < answer[1] - answer[0] else answer
+
+        if subsum > k:
+            start += 1
+        else:
+            end += 1
+
+    return answer
