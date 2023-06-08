09acc98396091206eb8decd0f0722dfb468a0f8c
eunna <dladmssk98@gmail.com>
2023-06-08 19:24:15 +0900

    level2 / 12905.py - add the 2nd solution

--- eunna/level2/12905.py
+++ eunna/level2/12905.py
@@ -10,3 +10,20 @@ def solution(board):
                 max_edge = max(max_edge, dp[i][j])
     answer = max_edge**2
     return answer
+
+
+def solution2(board):
+    answer = 0
+    edges = [[num for num in row] for row in board]
+    max_edge = max(edges[0])
+
+    for i in range(1, len(board)):
+        for j in range(1, len(board[0])):
+            if board[i][j] == 1:
+                edges[i][j] = (
+                    min(edges[i - 1][j], edges[i][j - 1], edges[i - 1][j - 1]) + 1
+                )
+                max_edge = max(edges[i][j], max_edge)
+
+    answer = max_edge**2
+    return answer
